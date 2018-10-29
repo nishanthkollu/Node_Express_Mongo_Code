dd2dde12681373cca77edf98b00b2338a91a363b
nishanthkollu <37909984+nishanthkollu@users.noreply.github.com>
2018-10-29 10:14:24 -0400

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ db.on('error', console.error.bind(console, 'MongoDB connection error:'));
 
 
 
-
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
